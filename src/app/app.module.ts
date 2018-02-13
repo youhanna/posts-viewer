abd8e541876d5ce00b734ad630591477003f7b53
Youhanna Mesiha <youhanna.mesiha@dh.com>
2018-02-13 15:53:04 -0500

    Add posts list and error components. Implement nav menu. Implement routing.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 
 import { AppComponent } from './app.component';
 import { NavMenuComponent } from './components/nav-menu/nav-menu.component';
 import { HomeComponent } from './components/home/home.component';
+import { PostsListComponent } from './components/posts-list/posts-list.component';
+import { ErrorComponent } from './components/error/error.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     NavMenuComponent,
-    HomeComponent
+    HomeComponent,
+    PostsListComponent,
+    ErrorComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot([
+      {path: 'posts', component: PostsListComponent},
+      {path: 'home', component: HomeComponent},
+      {path: 'error', component: ErrorComponent},
+      {path: '', redirectTo: 'home', pathMatch: 'full'},
+      {path: '**', redirectTo: 'error', pathMatch: 'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
